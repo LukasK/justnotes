b42d02a6734e040af3e3686c704873038a536316
Lukas Kircher <lukaskircher1@gmail.com>
2020-01-09 08:57:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# justnotes package
+
+Atom package for basic markdown note keeping with a little spice.
+
+- keep all your notes in markdown
+- convert single notes to html
+- convert directories including subtrees to html for a wiki-esque reading experience
+- link between files
+- etc.
+
+![A screenshot of your package](https://github.com/LukasK/justnotes/blob/master/misc/screen.png)
+# Prerequisites
+
+- pandoc installation
+
+# TODO
+
+- Finish README. What does it do? How? Why??!
+- Finish code todos
+- Add/update tests
+
+# Planned features
+
+- Convert individual files, not just folders
+- Insert toc in current markdown file at cursor position
---
+++ script/justnotes.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+# Deep-converts each .md file in a given directory to .html with pandoc.
+# Preserves linkes between .md files, css file references etc.
+#
+# Run `./gen_html.py --help` to display usage information.
+#
+# Example call:
+# > . gen_html.py -i markdown -o html -c style.css
+
+def main():
+    import logging
+    import argparse
+    import datetime
+    import sys
+    import shutil
+    import os
+    import subprocess
+    import re
+
+    parser = argparse.ArgumentParser(description='Create a copy of a directory tree where all .md files are replaced with .html files. I.e. create a website from your collection of .md notes.')
+    parser.add_argument("--verbose", help="activate verbose output", action="store_true")
+    parser.add_argument("--logfile", help="log file path - if none given, log is written to stdout")
+    # program parameters
+    parser.add_argument("src", metavar="SRC", help="source directory tree containing .md files")
+    parser.add_argument("dst", metavar="DST", help="destination for directory tree where .md have been replaced with .html files")
+    parser.add_argument("--css", help="css file")
+    parser.add_argument("--index", default=True, type=bool, help="create index.html file")
+    args = parser.parse_args()
+
+    loglevel = logging.WARNING
+    if args.verbose:
+        loglevel = logging.DEBUG
+    if args.logfile:
+        logging.basicConfig(filename=args.logfile, level=loglevel)
+    else:
+        logging.basicConfig(level=loglevel)
+    logging.info('--{} started-- {}'.format(sys.argv[0], datetime.datetime.now()))
+    logging.info('Arguments{{{}}}'.format(args))
+
+    inputDir = os.path.normpath(args.src)
+    outputDir = os.path.normpath(args.dst)
+
+    # deep copy notes root folder
+    shutil.copytree(inputDir, outputDir)
+
+    # add index file
+    indexpath = outputDir + "/index.md"
+    if args.index:
+        f = open(indexpath, 'w', encoding='utf-8')
+        f.close()
+
+    # get list of .md files
+    def deep_ls(dir, resultlist):
+        for entry in os.scandir(dir):
+            if entry.name.endswith('.md'):
+                resultlist += [entry]
+            elif entry.is_dir():
+                deep_ls(entry.path, resultlist)
+    mdfiles = []
+    deep_ls(outputDir, mdfiles)
+
+    # fill index file
+    if args.index:
+        sortedmdfiles = sorted(mdfiles, key=lambda x: x.path)
+        with open(indexpath, "w", encoding="utf-8") as idxout:
+            idxout.write('---\ntitle: Index\n---\n\n')
+            topic = None
+            for mdentry in sortedmdfiles:
+                if mdentry.path != indexpath:
+                    rootpath = mdentry.path
+                    prefix = outputDir + '/'
+                    if rootpath.find(prefix) == 0:
+                        rootpath = rootpath.replace(prefix, '', 1)
+                    # put non-top-level notes under their own heading
+                    slashidx = rootpath.find('/')
+                    deeprootpath = rootpath
+                    if slashidx > -1:
+                        newtopic = rootpath[0:slashidx]
+                        deeprootpath = rootpath[slashidx + 1:]
+                        if newtopic != topic:
+                            idxout.write('\n\n### {}\n\n'.format(newtopic))
+                            topic = newtopic
+                    idxout.write('- [{}]({})\n'.format(deeprootpath, rootpath))
+
+    # copy css file to output dir
+    cssfile = args.css
+    if cssfile:
+        cssfile = os.path.normpath(cssfile)
+        cssfilebase = os.path.basename(cssfile)
+        shutil.copy(cssfile, outputDir + '/')
+
+    # convert .md files to .html
+    for mdentry in mdfiles:
+        htmlfile = mdentry.path.replace('.md', '.html')
+
+        if cssfile:
+            # relative path from html file to css file in output root
+            csspath = os.path.relpath(outputDir + "/" + cssfilebase, start=os.path.dirname(htmlfile))
+            subprocess.run(['pandoc', '--standalone', mdentry.path, '--css', csspath, '--highlight-style=kate', '-o', htmlfile])
+        else:
+            subprocess.run(['pandoc', '--standalone', mdentry.path, '--highlight-style=kate', '-o', htmlfile])
+        # delete .md files of copy
+        os.remove(mdentry)
+        # change links within files to point from .md files to .html files
+        #    - find all      <a href="note1.md">
+        #      replace with  <ahref="note1.html">
+        tmpfile = htmlfile + ".tmp"
+        mdmatcher = re.compile(".md\">")
+        with open(htmlfile, "r", encoding="utf-8") as fi:
+            with open(tmpfile, "w", encoding="utf-8") as fo:
+                for line in fi:
+                    fo.write(re.subn(mdmatcher, ".html\">", line)[0])
+        os.replace(tmpfile, htmlfile)
+
+if __name__ == "__main__":
+    main()
